eeaa092188515bc3eef8bbea631042d8d761b2cb
siwhelan <simon@swhelan.dev>
2023-11-16 15:21:08 +0000

    added comments for clarity

--- lib/musictracker.py
+++ lib/musictracker.py
@@ -6,11 +6,17 @@ class MusicTracker:
     def add_track(self, artist, title):
         if not isinstance(artist, str) or not isinstance(title, str):
             raise Exception("Incorrect data! Please check Artist/Title.")
+
         if artist.strip() == "" or title.strip() == "":
             raise Exception("Missing data! Please check Artist/Title.")
+
+        # add the given track to the tracklist if it is formatted correctly
         self.tracklist.append((artist, title))
 
     def list_tracks(self):
+        # check the tracklist has contents, alert the user if not
         if not self.tracklist:
             return "List is empty!"
+
+        # return the full list of tracks, formatted in an easy to read style.
         return "\n".join([f"{artist}: {title}" for artist, title in self.tracklist])
